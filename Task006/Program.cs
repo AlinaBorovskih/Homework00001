5a6045c7cd8e432e79500289dcb9fe3336b22112
Alina_Borovskih <alinaberseneva@icloud.com>
2022-04-19 21:25:04 +0300

    Add task006

---
+++ Task006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+if ((numberA % 2) ==0)
+{
+    Console.WriteLine($"вывести чётное число {numberA}");
+}
+else
+{
+    Console.WriteLine($"вывести нечётное число {numberA}");
+}
